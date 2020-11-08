01c721de57519e0e50a2845a0cab9f3df7965c4f
Thor Summerall <thor.summerall@gmail.com>
2020-11-08 03:21:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
   index = input_to_index(input)
   
   if valid_move?(board, index) == true
-    move(board, index, value)
+    move(board, index)
     display_board(board)
   else
     return
